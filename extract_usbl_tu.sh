e48512b7f2c464ad54c42d530a73dfb87da2379d
uib <caterinamusi96@gmail.com>
2021-11-19 14:20:05 +0100

    scripts folder

--- extract_usbl_tu.sh
+++
@@ -1,7 +0,0 @@
-BAGFILE=$1
-
-rostopic echo -b  $BAGFILE -p /turbot/navigator/navigation->${BAGFILE%topi*}/turbot_navigation.csv
-rostopic echo -b  $BAGFILE -p /turbot/modem_delayed->${BAGFILE%topic*}/turbot_modem_delayed.csv
-rostopic echo -b  $BAGFILE -p /xiroi/usbllong->${BAGFILE%topic*}/turbot_usbllong.csv
-
-
--- extract_usbl_xi.sh
+++
@@ -1,5 +0,0 @@
-BAGFILE=$1
-
-rostopic echo -b $BAGFILE -p /xiroi/navigator/navigation->${BAGFILE%topi*}/xiroi_navigation.csv
-rostopic echo -b $BAGFILE -p /turbot/modem_delayed->${BAGFILE%topi*}/xiroi_modem_delayed.csv
-rostopic echo -b $BAGFILE -p /xiroi/usbllong->${BAGFILE%topi*}/xiroi_usbllong.csv
